ca14bd01bd7d421e6b5ca35434ec087892e792d7
MKushagra <kushagramisra10@gmail.com>
2017-12-07 00:21:13 +0530

    basic commit with pojos

---
+++ src/com/tokenmster/pojo/ServiceProvider.java
@@ -0,0 +1,73 @@
+package tokenmster.pojo;
+
+import java.net.URL;
+
+/**
+ * Created by kushagra on 12/6/2017.
+ */
+public class ServiceProvider
+{
+    String name;
+    String address;
+    String contact;
+    String imageLocation;
+    URL url;
+    String email;
+    String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getImageLocation() {
+        return imageLocation;
+    }
+
+    public void setImageLocation(String imageLocation) {
+        this.imageLocation = imageLocation;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
